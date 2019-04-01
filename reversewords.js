036e131d0b09f67c54ddc5b41b6f74ba23a5851b
Sean Gaston <46758834+SPGisMe@users.noreply.github.com>
2019-04-01 15:56:10 -0700

    Update reversewords.js

--- reversewords.js
+++ reversewords.js
@@ -2,8 +2,8 @@ function reverseWords(str) {
   let newStr = [];
   let subStr = ' ';
   
-  for (let i = str.length; i >= 0; i--) {
-    if (str[i - 1] === ' ' || i === 0){
+  for (let i = str.length; i >= 0; i--) { //loops by letter from end of sentence
+    if (str[i - 1] === ' ' || i === 0){ //every space (' ') sends a new reversed word to newStr array
       newStr.unshift(subStr);
       subStr = '';
     }
